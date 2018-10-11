321d6532f6900ce5ffdbcfdc9469e101c20a358d
Sergheev Andrian <sergheev@icloud.com>
2018-10-11 09:54:17 +0200

    Initial Commit

---
+++ BindingMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BindingMVVM
+//
+//  Created by Andrian Sergheev on 2018-10-11.
+//  Copyright © 2018 Andrian Sergheev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
